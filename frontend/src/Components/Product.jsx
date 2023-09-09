a0e3eb6413ffa6a57f735cb35279fcd40b6334cc
Burhaann <burhaann321@gmail.com>
2023-09-09 11:50:23 +0530

    Front End PART 1 Finished

--- frontend/src/Components/Product.jsx
+++ frontend/src/Components/Product.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
     <card className="my-3 p3 rounded">
-        <Link to={`/products/${product._id}`} >
-            <Card.Img src={product.image}  variant="top"/>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
         </Link>
-        <Card.Body>
-            <Link to={`/products/${product._id}`} >
-                <Card.Title as='div'>
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </Link>
-            <Card.Text as='h3'>
-                ${product.price}
-            </Card.Text>
-        </Card.Body>
-    </card>
-  )
-}
 
-export default Product
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </card>
+  );
+};
+
+export default Product;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
