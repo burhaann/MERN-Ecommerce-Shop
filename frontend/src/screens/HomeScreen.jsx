987a22c3bd1601ff207b7f6862c0d6db94544901
Burhaann <burhaann321@gmail.com>
2024-01-06 14:59:32 +0530

    Fetching now from MangoDB Atlas using Mongoose

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import Product from '../components/Product'
-import { Row, Col } from 'react-bootstrap'
-import products from '../products'
+import { useEffect, useState } from "react";
+import React from "react";
+import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
-    <>  
+    <>
       <h1>Latest Products</h1>
       <Row>
-          {products.map(product => (
-            <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-              <p>{product.description}</p>
-            </Col>
-          ))}
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+            {/* <p>{product.description}</p> */}
+          </Col>
+        ))}
       </Row>
     </>
-  
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
