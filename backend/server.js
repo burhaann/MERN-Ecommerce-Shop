2b07cdcdf07b7d2983eeff72fd2dc8b0940a4214
Burhaann <burhaann321@gmail.com>
2024-02-15 18:07:54 +0530

    Some Change for Deploying to Production

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie Parsre Middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Api is Running...");
-});
+// app.get("/", (req, res) => {
+//   res.send("Api is Running...");
+// });
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -37,6 +37,20 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  //any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
